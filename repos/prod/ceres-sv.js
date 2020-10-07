9fdff9f8eaa5ace284840b1327fac09344d57e60
ceresbakalite <ceres@users.noreply.github.com>
2020-10-08 02:43:15 +1100

    Update ceres-sv.js

--- repos/prod/ceres-sv.js
+++ repos/prod/ceres-sv.js
@@ -1,12 +1,12 @@
 export { ceres }
 
-var window.ceres = {};
+var ceres = {};
 (function(slideview)
 {
     'use strict';
 
-    ceres.tabImage = function(el) { window.open(el.getAttribute('src'), 'image'); }; // public method reference
-    ceres.getSlide = function(target, calc) { getSlide(csv.index = (calc) ? csv.index += target : target); };  // public method reference
+    slideview.tabImage = function(el) { window.open(el.getAttribute('src'), 'image'); }; // public method reference
+    slideview.getSlide = function(target, calc) { getSlide(csv.index = (calc) ? csv.index += target : target); };  // public method reference
 
     slideview.HTMLSlideViewElement = 'ceres-sv'; // required public element name
     slideview.HTMLImageListElement = 'ceres-csv'; // optional public markup noscript tag id when using embedded image lists
