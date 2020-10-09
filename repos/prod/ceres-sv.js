da00374d8b7e5bc82ffd8ed446a81b9de50f4340
ceresbakalite <ceres@users.noreply.github.com>
2020-10-10 01:59:16 +1100

    Update ceres-sv.js

--- repos/prod/ceres-sv.js
+++ repos/prod/ceres-sv.js
@@ -358,7 +358,7 @@ var ceres = {};
 
             if (touchstart != touchend)
             {
-                let targetIndex = (touchend < touchstart) ? csv.index : csv.index-1;
+                let targetIndex = (touchend < touchstart) ? csv.index + 1 : csv.index - 1;
                 getSlide(targetIndex, true);
             }
 
