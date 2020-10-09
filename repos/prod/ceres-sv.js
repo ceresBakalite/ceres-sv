7863df200fdd8e40a88c1eb26fce7bcee53dd8f7
ceresbakalite <ceres@users.noreply.github.com>
2020-10-09 23:08:01 +1100

    Update ceres-sv.js

--- repos/prod/ceres-sv.js
+++ repos/prod/ceres-sv.js
@@ -208,6 +208,8 @@ var ceres = {};
 
         inspect(resource.type.notify, resource.attribute.ProgenitorInnerHTML + csv.progenitor.innerHTML);
 
+        enableTouchSlide();
+
         function getSlideViewPointerContainer()
         {
             csv.progenitor.appendChild(document.createElement('br'));
@@ -254,6 +256,72 @@ var ceres = {};
         function getAccessibilityText()
         {
             return (arrayItem[1]) ? arrayItem[1].trim() : null;
+        }
+
+        function enableTouchSlide()
+        {
+            var container = document.querySelector(imageContainer);
+
+            container.addEventListener("touchstart", startTouch, false);
+            container.addEventListener("touchmove", moveTouch, false);
+
+            // Swipe Up / Down / Left / Right
+            var initialX = null;
+            var initialY = null;
+
+            function startTouch(e)
+            {
+                initialX = e.touches[0].clientX;
+                initialY = e.touches[0].clientY;
+            }
+
+            function moveTouch(e)
+            {
+                if (initialX === null || initialY === null) return;
+
+                var currentX = e.touches[0].clientX;
+                var currentY = e.touches[0].clientY;
+
+                var diffX = initialX - currentX;
+                var diffY = initialY - currentY;
+
+                if (Math.abs(diffX) > Math.abs(diffY))
+                {
+                    // sliding horizontally
+                    if (diffX > 0)
+                    {
+                        // swiped left
+                        console.log("swiped left");
+
+                    } else {
+
+                        // swiped right
+                        console.log("swiped right");
+                    }
+
+                } else {
+
+                    // sliding vertically
+                    if (diffY > 0)
+                    {
+                        // swiped up
+                        console.log("swiped up");
+
+                    } else {
+
+                        // swiped down
+                        console.log("swiped down");
+                    }
+
+                }
+
+                initialX = null;
+                initialY = null;
+
+                e.preventDefault();
+
+            }
+
         }
 
     }
