56e25c9e85ffabec78b040b76538fc1a41ad7d3d
ceresbakalite <ceres@users.noreply.github.com>
2020-10-14 20:02:59 +1100

    Update ceres-sv-lib.js

--- repos/prod/ceres-sv-lib.js
+++ repos/prod/ceres-sv-lib.js
@@ -17,7 +17,7 @@ var cereslibrary = {};
 
         if (!touch.act) touch.act = 10;
 
-        el.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; });
+        el.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; e.preventDefault()});
 
         el.addEventListener('touchend', e =>
         {
