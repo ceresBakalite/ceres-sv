9a9635948f8754f490f910e3cd8cfca8bab9397c
ceresbakalite <ceres@users.noreply.github.com>
2020-10-15 21:43:16 +1100

    Update ceres-sv-lib.js

--- repos/csvjs/mod/ceres-sv-lib.js
+++ repos/csvjs/mod/ceres-sv-lib.js
@@ -133,6 +133,8 @@ var cereslibrary = {};
 
     this.isEmptyOrNull = function(obj)
     {
+        if (!obj) return (!obj)
+        if (Object.keys(obj).length === 0) return (Object.keys(obj).length === 0);
         if (Array.isArray(obj)) return (obj.length === 0);
         if (obj === null || obj == 'undefined') return true;
         if (this.isString(obj)) return (obj.length === 0 || !obj.trim());
