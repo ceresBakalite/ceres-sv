b000e9d9749246a9633892c78f40020e5d6e1316
ceresbakalite <ceres@users.noreply.github.com>
2020-10-26 22:12:32 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -247,7 +247,8 @@ var ceres = {};
 
     function setSlideViewDisplay(attribute)
     {
-        const nodelist = document.querySelectorAll('img.slide, #' + csv.config.progenitor.id);
+        //const nodelist = document.querySelectorAll('img.slide, #' + csv.config.progenitor.id);
+        const nodelist = document.querySelectorAll('#' + csv.config.progenitor.id);
         nodelist.forEach(node => { node.style.display = attribute; } );
     }
 
