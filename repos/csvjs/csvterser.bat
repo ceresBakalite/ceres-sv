a08eb983c1ea1a5cfd979fd9b84931975ab99d79
ceresbakalite <ceres@users.noreply.github.com>
2020-10-29 00:59:51 +1100

    csv terser

---
+++ repos/csvjs/csvterser.bat
@@ -0,0 +1,2 @@
+terser C:\Users\Sandy\Documents\GitHub\ceres-sv\repos\csvjs\ceres-sv-lib.js -o C:\Users\Sandy\Documents\GitHub\ceres-sv\prod\ceres-sv-lib.min.js -c -m
+terser C:\Users\Sandy\Documents\GitHub\ceres-sv\repos\csvjs\ceres-sv.js -o C:\Users\Sandy\Documents\GitHub\ceres-sv\prod\ceres-sv.min.js -c -m
