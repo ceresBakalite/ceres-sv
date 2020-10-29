8219431bbacfaa804896b646c91132e43c00a2ea
ceresbakalite <ceres@users.noreply.github.com>
2020-10-29 23:51:10 +1100

    notification

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -102,7 +102,7 @@ var ceres = {};
         csr.listContainerMarkup = 'Image list markup ' + ((config.callback) ? 'delivered as promised by connectedCallback' : 'sourced from the document body') + ' [' + config.csvElement + ']:' + rsc.constant.newline;
         csr.bodyContentList = 'The ' + config.csvElement + ' src attribute url is unavailable. Searching for the fallback noscript image list content in the document body';
         csr.bodyContentListNotFound = 'Error: Unable to find the ' + config.csvElement + ' fallback noscript image list when searching the document body';
-        csr.configAttributes = 'The csv object attribute properties after initialisation [' + config.csvElement + ']: ';
+        csr.configAttributes = 'The ' + config.csvElement + ' element attributes after initialisation:' + rsc.constant.newline + rsc.constant.newline;
         csr.progenitorNotFound = 'Error: Unable to find the ' + config.csvElement + ' document element';
         csr.imageListNotFound = 'Error: Unable to find either the connectedCallback ' + config.csvElement + ' attribute source nor the fallback noscript image list container';
 
