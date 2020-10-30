43a94e63fe10d8a4eb95619aecdd06f1066d3d17
ceresbakalite <ceres@users.noreply.github.com>
2020-10-31 00:30:59 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -21,7 +21,7 @@ var ceres = {};
     const csv = 'ceres-sv'; // required element name
 
     this.getImage = function(el) { rsc.windowOpen({ element: el, type: 'image' }); }; // global scope method reference
-    this.getSlide = function(target, calc) { setSlide(this.slide = (calc) ? this.slide += target : target); };  // global scope method reference
+    this.getSlide = function(target, calc) { self.setSlide(this.slide = (calc) ? this.slide += target : target); };  // global scope method reference
 
     window.customElements.get(csv) || window.customElements.define(csv, class extends HTMLElement
     {
@@ -53,8 +53,6 @@ var ceres = {};
             config.progenitor = this;
             config.cache.css = [];
             config.cache.src = [];
-
-            config.progenitor.setSlide.bind(ceres);
 
             let css = this.getAttribute('css') || config.defaultCSS;
             let src = this.getAttribute('src') || null;
@@ -238,6 +236,8 @@ var ceres = {};
 
             function setSlide(target)
             {
+                var self = this;
+
                 console.log('hello from getSlide');
 
                 const slides = document.querySelectorAll('div.slideview');
