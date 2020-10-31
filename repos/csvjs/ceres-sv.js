88cce3f5e9b7db96da4fb8e06d120aa665b18263
ceresbakalite <ceres@users.noreply.github.com>
2020-10-31 20:44:21 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -81,18 +81,15 @@ var ceres = {};
 
             let precursor = function()
             {
-                if (rsc.isEmptyOrNull(csr))
-                {
-                    csr.listContainerMarkup = 'Image list markup ' + ((config.callback) ? 'delivered as promised by connectedCallback' : 'sourced from the document body') + ' [' + csv + ']:' + rsc.constant.newline;
-                    csr.bodyContentList = 'The ' + csv + ' src attribute url is unavailable. Searching for the fallback noscript image list content in the document body';
-                    csr.bodyContentListNotFound = 'Error: Unable to find the ' + csv + ' fallback noscript image list when searching the document body';
-                    csr.configAttributes = 'The ' + csv + ' element attributes after initialisation: ';
-                    csr.progenitorNotFound = 'Error: Unable to find the ' + csv + ' document element';
-                    csr.imageListNotFound = 'Error: Unable to find either the connectedCallback ' + csv + ' attribute source nor the fallback noscript image list container';
+                if (rsc.isEmptyOrNull(csr.listContainerMarkup)) csr.listContainerMarkup = 'Image list markup ' + ((config.callback) ? 'delivered as promised by connectedCallback' : 'sourced from the document body') + ' [' + csv + ']:' + rsc.constant.newline;
+                if (rsc.isEmptyOrNull(csr.bodyContentList)) csr.bodyContentList = 'The ' + csv + ' src attribute url is unavailable. Searching for the fallback noscript image list content in the document body';
+                if (rsc.isEmptyOrNull(csr.bodyContentListNotFound)) csr.bodyContentListNotFound = 'Error: Unable to find the ' + csv + ' fallback noscript image list when searching the document body';
+                if (rsc.isEmptyOrNull(csr.configAttributes)) csr.configAttributes = 'The ' + csv + ' element attributes after initialisation: ';
+                if (rsc.isEmptyOrNull(csr.progenitorNotFound)) csr.progenitorNotFound = 'Error: Unable to find the ' + csv + ' document element';
+                if (rsc.isEmptyOrNull(csr.imageListNotFound)) csr.imageListNotFound = 'Error: Unable to find either the connectedCallback ' + csv + ' attribute source nor the fallback noscript image list container';
 
-                    Object.freeze(csr);
-                }
-                
+                Object.freeze(csr);
+
                 return config.callback || config.noscript;
             }
 
