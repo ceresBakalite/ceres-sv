b52abf243bc5d60079110e1406cfe1c42bc8fd8e
ceresbakalite <ceres@users.noreply.github.com>
2020-10-31 18:25:52 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -24,7 +24,7 @@ var ceres = {};
 
     this.getImage = function(el) { rsc.windowOpen({ element: el, type: 'image' }); }; // global scope method reference
     //    this.getSlide = function(target, calc) { boundSetSlide.call(config.slide = (calc) ? config.slide += target : target); };  // global scope method reference
-    this.getSlide = function(target) { boundSetSlide.call(config.slide = target); };  // global scope method reference
+    this.getSlide = function(target) { config.slide = config.slide += target; boundSetSlide.call(); };  // global scope method reference
 //    this.getSlide = function(target, calc) { boundSetSlide.call(target, calc); };  // global scope method reference
 
     let csr = function() { return attribute; } // ceres slideview resource attributes
