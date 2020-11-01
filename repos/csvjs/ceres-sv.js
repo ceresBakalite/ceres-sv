275544a519dd3af804dd94e76aa5577d701cbd9b
ceresbakalite <ceres@users.noreply.github.com>
2020-11-01 14:34:13 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -19,18 +19,19 @@ var ceres = {};
 {
     'use strict';
 
+    const csv = 'ceres-sv'; // required ceres slideview element name
+
     this.getImage = function(el) { rsc.windowOpen({ element: el, type: 'image' }); }; // global scope method reference
     this.getSlide = function(target, calc) { config.bindSlide.call(config.slide = (calc) ? config.slide += target : target); };  // global scope method reference
 
-    const csv = 'ceres-sv'; // required ceres slideview element name
-    const cnv = 'ceres-csv'; // optional markup noscript tag id when using an embedded image list
-    const csr = function() { return attribute; } // ceres slideview resource attributes
-    const config = new class { constructor(){} } // ceres slideview configuration attributes
-
-    window.customElements.get(csv) || window.customElements.define(csv, class extends HTMLElement
+    window.customElements.get('ceres-sv') || window.customElements.define('ceres-sv', class extends HTMLElement
     {
         async connectedCallback()
         {
+            const cnv = 'ceres-csv'; // optional markup noscript tag id when using an embedded image list
+            const csr = function() { return attribute; } // ceres slideview resource attributes
+            const config = new class { constructor(){} } // ceres slideview configuration attributes
+
             if (!Object.isFrozen(csr)) getResources();
 
             let css = this.getAttribute('css') || config.defaultCSS;
