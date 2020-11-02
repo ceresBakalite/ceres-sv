1dabcf7dd330466c8a5cf9469df681d9af618ca1
ceresbakalite <ceres@users.noreply.github.com>
2020-11-02 15:43:52 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -30,12 +30,12 @@ var ceres = {};
         className.set('right', config.slide + 1);
         className.set('nub', Number.parseInt(el.id.replace(csv + '-nub', ''), 10));
 
-        console.log('el.value: ' + Number.parseInt(el.csv, 10));
+        config.slide = className.get(el.className);
         config.slide = className.get(el.className);
     }
 
     this.getImage = function(el) { rsc.windowOpen({ element: el, type: 'image' }); }; // global scope method reference
-    this.getSlide = function(el) { config.bindSlide.call(setSlide(el)); };  // global scope method reference
+    this.getSlide = function(index) { config.bindSlide.call(config.slide = index); };  // global scope method reference
 
     window.customElements.get(csv) || window.customElements.define(csv, class extends HTMLElement
     {
@@ -208,8 +208,8 @@ var ceres = {};
                     if (config.attributes.sub) rsc.composeElement({ el: 'div', id: elements.subName, classValue: 'subtitle', parent: slideContainer, markup: getSubtitle() });
                 }
 
-                rsc.composeElement({ el: 'a', id: csv + '-left', classValue: 'left', parent: imageContainer, markup: '&#10094;', csv: config.slide - 1, onClickEvent: 'window.getSlide(this)' });
-                rsc.composeElement({ el: 'a', id: csv + '-right', classValue: 'right', parent: imageContainer, markup: '&#10095;', csv: config.slide + 1, onClickEvent: 'window.getSlide(this)' });
+                rsc.composeElement({ el: 'a', id: csv + '-left', classValue: 'left', parent: imageContainer, markup: '&#10094;', onClickEvent: 'window.getSlide(' + config.slide - 1 + ')' });
+                rsc.composeElement({ el: 'a', id: csv + '-right', classValue: 'right', parent: imageContainer, markup: '&#10095;', onClickEvent: 'window.getSlide( + config.slide + 1 + )' });
 
                 if (config.attributes.nub) getSlideViewTrackContainer();
 
@@ -228,7 +228,7 @@ var ceres = {};
 
                 function getSlideViewTrackContainer()
                 {
-                    const getClickEvent = function() { return 'window.getSlide(this)'; }
+                    const getClickEvent = function() { return 'window.getSlide(' + index + ')'; }
 
                     const trackContainer = document.createElement('div');
                     trackContainer.id = csv + '-nub';
@@ -239,7 +239,7 @@ var ceres = {};
                     for (let item = 0; item < config.imageArray.length; item++)
                     {
                         var index = item + 1;
-                        rsc.composeElement({ el: 'span', id: csv + '-nub' + index, classValue: 'nub', parent: trackContainer, csv: index, onClickEvent: getClickEvent() });
+                        rsc.composeElement({ el: 'span', id: csv + '-nub' + index, classValue: 'nub', parent: trackContainer, onClickEvent: getClickEvent() });
                     }
 
                     bodyContainer.appendChild(document.createElement('br'));
