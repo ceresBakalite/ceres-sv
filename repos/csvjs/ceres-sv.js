a49e11e97aeac456b4bb24c142f3ddf99be93f0b
ceresbakalite <ceres@users.noreply.github.com>
2020-11-03 11:57:37 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -204,7 +204,9 @@ window.ceres = {};
     const csv = 'ceres-sv'; // required ceres slideview element name
     const cns = 'ceres-csv'; // optional markup noscript tag id when using an embedded image list
 
-    test1.test('ceres say hi');
+    let t1 = new test1();
+
+    t1.test('ceres say hi');
 
     window.customElements.get(csv) || window.customElements.define(csv, class extends HTMLElement
     {
