3f7945318b9aa17aa1402dceb70e8bb1460257db
ceresbakalite <ceres@users.noreply.github.com>
2020-11-03 19:58:42 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -335,15 +335,6 @@ window.ceres = {};
 
                 });
 
-                cfg.cache.css.forEach(item =>
-                {
-                    fetch(item).then(response => response.text()).then(str =>
-                    {
-                        styleContainer.insertAdjacentHTML('beforeend', str)
-                    });
-
-                });
-
                 const bodyContainer = document.createElement('div');
                 bodyContainer.id = csv + '-body';
                 bodyContainer.className = 'slideview-body';
