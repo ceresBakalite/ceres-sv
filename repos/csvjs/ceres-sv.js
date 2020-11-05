faa77abb836b070e9bda213f38c22abb66977b6f
ceresbakalite <ceres@users.noreply.github.com>
2020-11-06 02:25:28 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -371,10 +371,10 @@ window.ceres = {};
             function getSlideView()
             {
                 let getURL = function() { return (!rsc.isEmptyOrNull(arrayItem[0])) ? arrayItem[0].trim() : null; }
-                let getSurtitle = function(i) { return (cfg.attrib.sur) ? i + ' / ' + cfg.imageArray.length : null; }
+                let getSurtitle = function(index) { return (cfg.attrib.sur) ? index + ' / ' + cfg.imageArray.length : null; }
                 let getSubtitle = function() { return (cfg.attrib.sub) ? getAccessibilityText() : null; }
                 let getAccessibilityText = function() { return (!rsc.isEmptyOrNull(arrayItem[1])) ? arrayItem[1].trim() : null; }
-                let setTrackId = function(i) { return 'nub' + i; }
+                let setTrackId = function(index) { return 'nub' + index; }
 
                 cfg.attrib.shade = document.querySelector('#' + progenitor.id);
 
