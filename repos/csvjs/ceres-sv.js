c93ec792430c9471b63bbb9301390ff4c926e559
ceresbakalite <ceres@users.noreply.github.com>
2020-11-08 04:11:18 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -255,7 +255,8 @@ window.ceres = {};
                         {
                             const ar = cfg.attrib.auto.replace(rsc.constant.whitespace,'').split(',');
 
-                            cfg.attrib.auto.forEach(key => { delete cfg.attrib.auto[key]; });
+                            //cfg.attrib.auto.forEach(key => { delete cfg.attrib.auto[key]; });
+                            cfg.attrib.auto = {};
 
                             if (ar[0].toLocaleLowerCase(locale) == 'false') return;
 
