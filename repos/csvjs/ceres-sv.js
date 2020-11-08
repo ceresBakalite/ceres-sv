e48d6ce404a9e0bdd47457c5812ca68fbc99c101
ceresbakalite <ceres@users.noreply.github.com>
2020-11-08 22:44:22 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -282,7 +282,7 @@ window.ceres = {};
                             }
 
                             cfg.attrib.autocycle = Number.isInteger(parseInt(ar[0])) ? parseInt(ar[0]) : 10;
-                            cfg.attrib.autopause = Number.isInteger(parseInt(ar[1])) ? parseInt(ar[1]) : 10000;
+                            cfg.attrib.autopause = Number.isInteger(parseInt(ar[1])) ? parseInt(ar[1]) : 3000;
                             cfg.attrib.autostop = cfg.attrib.autocycle > 0;
 
                             return false;
