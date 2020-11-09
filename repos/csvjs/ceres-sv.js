0d2709fca84e625b9941e093d61d3c2a0f5ed4ac
ceresbakalite <ceres@users.noreply.github.com>
2020-11-10 07:43:35 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -122,7 +122,7 @@ window.ceres = {};
 
                     let slideContainer = document.createElement('div');
                     slideContainer.id = 'img' + index;
-                    slideContainer.className = atr.setViewFade();
+                    slideContainer.className = cfg.attrib.zoom ? atr.setViewFade('view zoom fade none') : atr.setViewFade('view fade none');
 
                     imageContainer.appendChild(slideContainer);
 
@@ -178,8 +178,8 @@ window.ceres = {};
                 if (rsc.isEmptyOrNull(slides[enable])) return;
 
                 const el = shadow.querySelector('div.slideview-image > div.pointer');
-                if (el) el.className = atr.setViewFade();
-                slides[enable].className = atr.setViewFade(true);
+                if (el) el.className = cfg.attrib.zoom ? atr.setViewFade('view zoom fade none') : atr.setViewFade('view fade none');
+                slides[enable].className = cfg.attrib.zoom ? atr.setViewFade('view zoom fade pointer') : atr.setViewFade('view fade pointer');
 
                 if (cfg.attrib.nub && cfg.attrib.static) setNubStyle();
             }
@@ -254,14 +254,7 @@ window.ceres = {};
                 (function() {
 
                     atr.precursor = function() { return cfg.fetchsrc || cfg.noscript; }
-
-                    atr.setViewFade = function(type)
-                    {
-                        let className = cfg.attrib.zoom ? 'view zoom' : 'view';
-                        className = cfg.attrib.autofade ? className +=  ' fade' : className;
-
-                        return type ? className +=  ' pointer' : className +=  ' none';
-                    }
+                    atr.setViewFade = function(className) { return cfg.attrib.autofade ? className : className.replace('fade ', ''); }
 
                     atr.shadowSlide = function(node)
                     {
