c1bd6c21db4b772c6e10015a62fb70ddd6ab833e
ceresbakalite <ceres@users.noreply.github.com>
2020-11-13 04:44:39 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -143,8 +143,10 @@ window.ceres = {};
                     const getClickEvent = function() { return 'ceres.getSlide(this)'; }
                     const getActiveState = function(className) { return !cfg.attrib.nub || cfg.attrib.static ? className : className += ' none'; }
                     const cacheAvailable = ('caches' in window);
-                    const note = setNotifications(); // notification strings
+                    const note = {}; // notification strings
                     const srm = new Map(); // shadowroot manager
+
+                    setNotifications();
 
                     atr.fetchStylesheets = function(str)
                     {
@@ -248,14 +250,16 @@ window.ceres = {};
                     {
                         if (!cacheAvailable) return;
 
-                        const cacheName = csv + '-cache';
-                        const caching = setCaching(); // http cache allocation
+                        const caching = {}; // http cache allocation
 
+                        setCache();
+
+                        const cacheName = csv + '-cache';
                         cfg.cache.script = [ rsc.getImportMetaUrl() ];
 
                         caching.installCache(cacheName, rsc.removeDuplcates(cfg.cache.css.concat(cfg.cache.src.concat(cfg.cache.script))));
 
-                        function setCaching()
+                        function setCache()
                         {
                             // caching;
                             (function(cache) {
