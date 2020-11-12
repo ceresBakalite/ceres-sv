ccb95237e0a10a1142951bb0b38e83e53a3eb46a
ceresbakalite <ceres@users.noreply.github.com>
2020-11-12 20:22:29 +1100

    css

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -123,7 +123,7 @@ window.ceres = {};
                 {
 
                     cfg.bodyContainer.className = 'slideview-body view';
-                    cfg.bodyContainer.style.display  = 'block';
+                    //cfg.bodyContainer.style.display  = 'block';
                     //rsc.setDisplayStyle('div.slideview-body', 'block');
 
                 }, cfg.attrib.delay);
@@ -186,7 +186,7 @@ window.ceres = {};
                         cfg.bodyContainer = document.createElement('div');
                         cfg.bodyContainer.id = csv + '-body';
                         cfg.bodyContainer.className = 'slideview-body none';
-                        cfg.bodyContainer.style.display  = 'none';
+                        //cfg.bodyContainer.style.display  = 'none';
 
                         cfg.shade.appendChild(cfg.bodyContainer);
                     }
