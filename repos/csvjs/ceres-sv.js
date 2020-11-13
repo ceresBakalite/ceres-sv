87a0265ed80e1a8c9a7100d2e8499a9a809f2b37
ceresbakalite <ceres@users.noreply.github.com>
2020-11-13 17:48:53 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -9,6 +9,198 @@
  *
  * Copyright (c) 2020 Alexander Munro
 */
+const caching = {}; // http cache allocation
+(function(cache) {
+
+    caching.available = ('caches' in window);
+
+    caching.listExistingCacheNames = function()
+    {
+        caches.keys().then(function(cacheKeys) { console.log('listCache: ' + cacheKeys); });
+    }
+
+    caching.installCache = function(namedCache, urlArray, urlImage = '/images/NAVCogs.png')
+    {
+        window.addEventListener('install', function(e) { e.waitUntil(caches.open(namedCache).then(function(cache) { return cache.addAll(urlArray); })); });
+
+        window.addEventListener('fetch', function(e)
+        {
+            e.respondWith(caches.match(e.request).then(function(response)
+            {
+                if (response !== undefined) return response;
+
+                return fetch(e.request).then(function (response)
+                {
+                    let responseClone = response.clone();
+                    caches.open(namedCache).then(function (cache) { cache.put(e.request, responseClone); });
+                    return response;
+
+                }).catch(function () {
+
+                    return caches.match(urlImage);
+
+                });
+
+            }));
+
+        });
+
+    }
+
+})(); // end caching
+
+const rsc = {}; // generic resource allocation
+(function() {
+
+    const protean = {},
+    resource = {},
+    symbol = new Map();
+
+    initialise();
+
+    rsc.constant = protean; // exposed local scope attributes
+
+    Object.freeze(rsc.constant);
+
+    rsc.srcOpen = function(obj) { window.open(obj.element.getAttribute('src'), obj.type); }
+    rsc.isString = function(obj) { return Object.prototype.toString.call(obj) == '[object String]'; }
+    rsc.clearElement = function(el) { while (el.firstChild) el.removeChild(el.firstChild); }
+    rsc.getImportMetaUrl = function() { return import.meta.url; }
+
+    rsc.composeElement = function(el)
+    {
+        const precursor = el.parent;
+        const node = document.createElement(el.typeof);
+
+        if (el.id) node.setAttribute("id", el.id);
+        if (el.className) node.setAttribute("class", el.className);
+        if (el.onClick) node.setAttribute("onclick", el.onClick);
+        if (el.src) node.setAttribute("src", el.src);
+        if (el.alt) node.setAttribute("alt", el.alt);
+        if (el.markup) node.insertAdjacentHTML('afterbegin', el.markup);
+
+        precursor.appendChild(node);
+    }
+
+    rsc.setHorizontalSwipe = function(touch, callback, args)
+    {
+        if (!touch.act) touch.act = 80;
+
+        touch.node.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; }, { passive: true } );
+        touch.node.addEventListener('touchmove', e => { e.preventDefault(); }, { passive: true });
+        touch.node.addEventListener('touchend', e =>
+        {
+            touch.end = e.changedTouches[0].screenX;
+
+            if (Math.abs(touch.start - touch.end) > touch.act)
+            {
+                args.action = (touch.start > touch.end);
+                callback.call(this, args);
+            }
+
+        }, { passive: true });
+
+    }
+
+    rsc.isEmptyOrNull = function(obj)
+    {
+        if (obj === null || obj == 'undefined') return true;
+
+        if (rsc.isString(obj)) return (obj.length === 0 || !obj.trim());
+        if (Array.isArray(obj)) return (obj.length === 0);
+        if (obj && obj.constructor === Object) return (Object.keys(obj).length === 0);
+
+        return !obj;
+    }
+
+    rsc.getBooleanAttribute = function(attribute, locale = 'en')
+    {
+        if (attribute === true || attribute === false) return attribute;
+        if (rsc.isEmptyOrNull(attribute)) return false;
+        if (!rsc.isString(attribute)) return false;
+
+        const token = attribute.trim().toLocaleLowerCase(locale);
+
+        return symbol.get(token) || false;
+    }
+
+    rsc.getUniqueElementId = function(str = null, range = 100)
+    {
+        let elName = function() { return str + Math.floor(Math.random() * range) };
+        let el = null;
+
+        while (document.getElementById(el = elName())) {};
+
+        return el;
+    }
+
+    rsc.removeDuplcates = function(obj, sort)
+    {
+        const key = JSON.stringify;
+        let ar = [...new Map (obj.map(node => [key(node), node])).values()];
+
+        return sort ? ar.sort((a, b) => a - b) : ar;
+    }
+
+    rsc.inspect = function(diagnostic)
+    {
+        if (rsc.isEmptyOrNull(diagnostic)) return rsc.inspect({ type: protean.error, notification: resource.inspect });
+
+        const errorHandler = function(error)
+        {
+            if (rsc.isEmptyOrNull(error)) return rsc.inspect({ type: protean.error, notification: resource.errorHandler });
+
+            const err = error.notification + ' [ DateTime: ' + new Date().toLocaleString() + ' ]';
+            console.error(err);
+
+            if (error.alert) alert(err);
+
+            return false;
+        }
+
+        const lookup = {
+            [protean.notify]: function() { if (diagnostic.logtrace) console.info(diagnostic.notification); },
+            [protean.error]: function() { errorHandler({ notification: diagnostic.notification, alert: diagnostic.logtrace } ); },
+            [protean.reference]: function() { if (diagnostic.logtrace) console.log('Reference: ' + protean.newline + protean.newline + diagnostic.reference); },
+            [protean.default]: function() { errorHandler({ notification: resource.errordefault, alert: diagnostic.logtrace } ); }
+        };
+
+        return lookup[diagnostic.type]() || lookup[protean.default];
+    }
+
+    rsc.getObjectProperties = function(object, str = '')
+    {
+        for (let property in object) str += property + ': ' + object[property] + ', ';
+        return str.replace(/, +$/g,'');
+    }
+
+    function initialise()
+    {
+        symbol.set('true', true);
+        symbol.set('t', true);
+        symbol.set('yes', true);
+        symbol.set('y', true);
+        symbol.set('1', true);
+
+        protean.reference = 1;
+        protean.notify = 2;
+        protean.default = 98;
+        protean.error = 99;
+        protean.isWindows = (navigator.appVersion.indexOf('Win') != -1);
+        protean.newline = protean.isWindows ? '\r\n' : '\n';
+        protean.whitespace = /\s/g;
+
+        Object.freeze(protean);
+
+        resource.inspect = 'Error: An exception occurred in the inspect method.  The diagnostic argument was empty or null';
+        resource.errorhandler = 'Error: An exception occurred in the errorhandler method.  The error argument was empty or null';
+        resource.errordefault = 'An unexpected error has occurred. The inspection type was missing or invalid';
+
+        Object.freeze(resource);
+    }
+
+})(); // end resource allocation
+
 window.ceres = {};
 (function()
 {
@@ -16,198 +208,6 @@ window.ceres = {};
 
     const csv = 'ceres-sv'; // required ceres slideview element name
     const cns = 'ceres-csv'; // optional markup noscript tag id when using an embedded image list
-
-    const caching = {}; // http cache allocation
-    (function(cache) {
-
-        caching.available = ('caches' in window);
-
-        caching.listExistingCacheNames = function()
-        {
-            caches.keys().then(function(cacheKeys) { console.log('listCache: ' + cacheKeys); });
-        }
-
-        caching.installCache = function(namedCache, urlArray, urlImage = '/images/NAVCogs.png')
-        {
-            window.addEventListener('install', function(e) { e.waitUntil(caches.open(namedCache).then(function(cache) { return cache.addAll(urlArray); })); });
-
-            window.addEventListener('fetch', function(e)
-            {
-                e.respondWith(caches.match(e.request).then(function(response)
-                {
-                    if (response !== undefined) return response;
-
-                    return fetch(e.request).then(function (response)
-                    {
-                        let responseClone = response.clone();
-                        caches.open(namedCache).then(function (cache) { cache.put(e.request, responseClone); });
-                        return response;
-
-                    }).catch(function () {
-
-                        return caches.match(urlImage);
-
-                    });
-
-                }));
-
-            });
-
-        }
-
-    })(); // end caching
-
-    const rsc = {}; // generic resource allocation
-    (function() {
-
-        const protean = {},
-        resource = {},
-        symbol = new Map();
-
-        initialise();
-
-        rsc.constant = protean; // exposed local scope attributes
-
-        Object.freeze(rsc.constant);
-
-        rsc.srcOpen = function(obj) { window.open(obj.element.getAttribute('src'), obj.type); }
-        rsc.isString = function(obj) { return Object.prototype.toString.call(obj) == '[object String]'; }
-        rsc.clearElement = function(el) { while (el.firstChild) el.removeChild(el.firstChild); }
-        rsc.getImportMetaUrl = function() { return import.meta.url; }
-
-        rsc.composeElement = function(el)
-        {
-            const precursor = el.parent;
-            const node = document.createElement(el.typeof);
-
-            if (el.id) node.setAttribute("id", el.id);
-            if (el.className) node.setAttribute("class", el.className);
-            if (el.onClick) node.setAttribute("onclick", el.onClick);
-            if (el.src) node.setAttribute("src", el.src);
-            if (el.alt) node.setAttribute("alt", el.alt);
-            if (el.markup) node.insertAdjacentHTML('afterbegin', el.markup);
-
-            precursor.appendChild(node);
-        }
-
-        rsc.setHorizontalSwipe = function(touch, callback, args)
-        {
-            if (!touch.act) touch.act = 80;
-
-            touch.node.addEventListener('touchstart', e => { touch.start = e.changedTouches[0].screenX; }, { passive: true } );
-            touch.node.addEventListener('touchmove', e => { e.preventDefault(); }, { passive: true });
-            touch.node.addEventListener('touchend', e =>
-            {
-                touch.end = e.changedTouches[0].screenX;
-
-                if (Math.abs(touch.start - touch.end) > touch.act)
-                {
-                    args.action = (touch.start > touch.end);
-                    callback.call(this, args);
-                }
-
-            }, { passive: true });
-
-        }
-
-        rsc.isEmptyOrNull = function(obj)
-        {
-            if (obj === null || obj == 'undefined') return true;
-
-            if (rsc.isString(obj)) return (obj.length === 0 || !obj.trim());
-            if (Array.isArray(obj)) return (obj.length === 0);
-            if (obj && obj.constructor === Object) return (Object.keys(obj).length === 0);
-
-            return !obj;
-        }
-
-        rsc.getBooleanAttribute = function(attribute, locale = 'en')
-        {
-            if (attribute === true || attribute === false) return attribute;
-            if (rsc.isEmptyOrNull(attribute)) return false;
-            if (!rsc.isString(attribute)) return false;
-
-            const token = attribute.trim().toLocaleLowerCase(locale);
-
-            return symbol.get(token) || false;
-        }
-
-        rsc.getUniqueElementId = function(str = null, range = 100)
-        {
-            let elName = function() { return str + Math.floor(Math.random() * range) };
-            let el = null;
-
-            while (document.getElementById(el = elName())) {};
-
-            return el;
-        }
-
-        rsc.removeDuplcates = function(obj, sort)
-        {
-            const key = JSON.stringify;
-            let ar = [...new Map (obj.map(node => [key(node), node])).values()];
-
-            return sort ? ar.sort((a, b) => a - b) : ar;
-        }
-
-        rsc.inspect = function(diagnostic)
-        {
-            if (rsc.isEmptyOrNull(diagnostic)) return rsc.inspect({ type: protean.error, notification: resource.inspect });
-
-            const errorHandler = function(error)
-            {
-                if (rsc.isEmptyOrNull(error)) return rsc.inspect({ type: protean.error, notification: resource.errorHandler });
-
-                const err = error.notification + ' [ DateTime: ' + new Date().toLocaleString() + ' ]';
-                console.error(err);
-
-                if (error.alert) alert(err);
-
-                return false;
-            }
-
-            const lookup = {
-                [protean.notify]: function() { if (diagnostic.logtrace) console.info(diagnostic.notification); },
-                [protean.error]: function() { errorHandler({ notification: diagnostic.notification, alert: diagnostic.logtrace } ); },
-                [protean.reference]: function() { if (diagnostic.logtrace) console.log('Reference: ' + protean.newline + protean.newline + diagnostic.reference); },
-                [protean.default]: function() { errorHandler({ notification: resource.errordefault, alert: diagnostic.logtrace } ); }
-            };
-
-            return lookup[diagnostic.type]() || lookup[protean.default];
-        }
-
-        rsc.getObjectProperties = function(object, str = '')
-        {
-            for (let property in object) str += property + ': ' + object[property] + ', ';
-            return str.replace(/, +$/g,'');
-        }
-
-        function initialise()
-        {
-            symbol.set('true', true);
-            symbol.set('t', true);
-            symbol.set('yes', true);
-            symbol.set('y', true);
-            symbol.set('1', true);
-
-            protean.reference = 1;
-            protean.notify = 2;
-            protean.default = 98;
-            protean.error = 99;
-            protean.isWindows = (navigator.appVersion.indexOf('Win') != -1);
-            protean.newline = protean.isWindows ? '\r\n' : '\n';
-            protean.whitespace = /\s/g;
-
-            Object.freeze(protean);
-
-            resource.inspect = 'Error: An exception occurred in the inspect method.  The diagnostic argument was empty or null';
-            resource.errorhandler = 'Error: An exception occurred in the errorhandler method.  The error argument was empty or null';
-            resource.errordefault = 'An unexpected error has occurred. The inspection type was missing or invalid';
-
-            Object.freeze(resource);
-        }
-
-    })(); // end resource allocation
 
     window.customElements.get(csv) || window.customElements.define(csv, class extends HTMLElement
     {
