5ad0f967bde4e9d6cef02603885b91f41b8a4e2a
ceresbakalite <ceres@users.noreply.github.com>
2020-11-15 21:46:26 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -398,6 +398,7 @@ window.ceres = {};
                             cfg.attrib.autocancel = cfg.attrib.autocycle > -1;
 
                             cfg.attrib.fade = cfg.attrib.autopause > 400;
+                            cfg.attrib.nub = 'hidden';
 
                             return false;
                         }
