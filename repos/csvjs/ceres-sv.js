b7e8bfb06580ca465e3b9490d1847f63bca5e4ad
ceresbakalite <ceres@users.noreply.github.com>
2020-11-16 09:09:13 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -105,7 +105,9 @@ window.ceres = {};
         this.htmlToText = function(html, regex)
         {
             if (this.isEmptyOrNull(html)) return;
-            if (regex) return html.replace(this.markup, '');
+
+            let template = html.includes('</template>');
+            if (regex || template) return html.replace(this.markup, '');
 
             let doc = new DOMParser().parseFromString(html, 'text/html');
             return doc.body.textContent || doc.body.innerText;
@@ -211,7 +213,7 @@ window.ceres = {};
             cfg.fetchsrc = !rsc.isEmptyOrNull(src);
 
             if (cfg.fetchcss) atr.fetchStylesheets(css);
-            if (cfg.fetchsrc) csvNode.insertAdjacentHTML('afterbegin', rsc.htmlToText( await ( await fetch(src) ).text(), true ) );
+            if (cfg.fetchsrc) csvNode.insertAdjacentHTML('afterbegin', rsc.htmlToText( await ( await fetch(src) ).text(), false ) );
 
             cfg.cache.src = cfg.cache.src.concat(src);
 
