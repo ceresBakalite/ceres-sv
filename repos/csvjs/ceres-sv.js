75cac9c64ffdcf3ca3844effdb69c0a41ef2aed6
ceresbakalite <ceres@users.noreply.github.com>
2020-11-15 16:41:27 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -479,7 +479,7 @@ window.ceres = {};
                     rsc.notify = 2;
                     rsc.default = 98;
                     rsc.error = 99;
-                    rsc.bool = ['TRUE','true','1','YES','yes','Y','y','T','t'];
+                    rsc.bool = ['true','1','yes','on','y','t','TRUE','YES','ON','Y','T'];
                     rsc.isWindows = (navigator.appVersion.indexOf('Win') != -1);
                     rsc.newline = rsc.isWindows ? '\r\n' : '\n';
                     rsc.whitespace = /\s/g;
