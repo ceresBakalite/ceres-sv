cb0100646b39eeece39b7a526e538c4e96cdc2e1
ceresbakalite <ceres@users.noreply.github.com>
2020-11-16 23:01:07 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -159,7 +159,6 @@ window.ceres = {};
 
         this.installCache = function(namedCache, urlArray, urlImage = '/images/NAVCogs.png')
         {
-            console.log('installCache');
             window.addEventListener('install', function(e) { e.waitUntil(caches.open(namedCache).then(function(cache) { return cache.addAll(urlArray); })); });
 
             window.addEventListener('fetch', function(e)
