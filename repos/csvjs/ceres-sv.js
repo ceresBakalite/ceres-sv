0ef465a5d2b6423b8f68cd65f9e98c10d4628916
ceresbakalite <ceres@users.noreply.github.com>
2020-11-20 12:37:18 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -103,7 +103,7 @@ window.ceres = {};
             let jsonList = function(list = '')
             {
                 let response = JSON.parse(obj.text);
-                for (let i = 0; i < response.data.length; i++) { list += response.data[i].url + ', ' + response.data[i].text + rsc.newline; }
+                for (let i = 0; i < response.data.length; i++) { list += response.data[i].url + ', ' + response.data[i].text + ' '; }
                 return list;
             }
 
