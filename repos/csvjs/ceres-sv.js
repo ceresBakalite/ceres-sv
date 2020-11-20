8d3691f1d2e3fe1281198fdc2a04ccba232ac2cc
ceresbakalite <ceres@users.noreply.github.com>
2020-11-20 16:13:46 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -173,7 +173,7 @@ window.ceres = {};
             function configureAttributes()
             {
                 cfg.defaultCSS = 'https://ceresbakalite.github.io/ceres-sv/prod/ceres-sv.min.css'; // the default slideview stylesheet
-                cfg.src = String(csvNode.getAttribute('src')).trim() || null;
+                cfg.src = String(csvNode.getAttribute('src')).trim();
                 cfg.css = csvNode.getAttribute('css') || cfg.defaultCSS;
                 cfg.fetchsrc = !rsc.isEmptyOrNull(cfg.src);
                 cfg.attrib = {};
