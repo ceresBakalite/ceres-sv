6a64cede339f30809bdac835254c2c917e903efb
ceresbakalite <ceres@users.noreply.github.com>
2020-11-30 20:41:32 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -320,6 +320,7 @@ window.ceres = {};
                                             //if (elStyle.property.includes(item) || colorAttribute(item))
                                             if (colorAttribute(item))
                                             {
+                                                console.log('not found: ' + item);
                                                 console.log('this found: ' + item.replace('COLOR:',''));
                                             }
                                             //if (elStyle.property.includes(item) || item.includes('COLOR:') || item.includes('#') || item.includes('rgb(') || item.includes('rgba(' || item.includes('hsl(') || item.includes('hsla('))) console.log('found: ' + item.replace('COLOR:',''));
