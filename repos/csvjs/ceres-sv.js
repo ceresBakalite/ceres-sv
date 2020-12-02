3d428338b5fc648fbe7ef72f27c238e701e1db14
ceresbakalite <ceres@users.noreply.github.com>
2020-12-02 16:51:48 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -316,7 +316,7 @@ window.ceres = {};
                                                     let text = cfg.cssNode.textContent;
 
                                                     let regex = attribute == 'sur' ? /.surtitle[^&]*?}/i : /.subtitle[^&]*?}/i;
-                                                    let group = text.match(regex).trim();
+                                                    let group = text.match(regex) + '';
                                                     let value = item.replace('COLOR:', '');
 
                                                     //cfg.shadowStyle = cfg.shadowStyle.match(regex).replace(/color[^&]*?;/i, 'color: ' + value + ';');
