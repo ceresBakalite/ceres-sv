e61659d82aabc611c9e385c1740e60ece34ca7fb
ceresbakalite <ceres@users.noreply.github.com>
2020-12-05 20:36:05 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -172,7 +172,7 @@ window.ceres = {};
 
                 for (const group in groups)
                 {
-                    console.log('group: ' + group.replace(/"/g, 'zz'));
+                    console.log('group: ' + group.replace(/"/g, '&#39;') + '');
                 }
 
             });
