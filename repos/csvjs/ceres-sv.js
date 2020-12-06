bd5c32104e959c4c1c5117da59de26f216347add
ceresbakalite <ceres@users.noreply.github.com>
2020-12-06 21:39:18 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -119,12 +119,12 @@ window.ceres = {};
 
             textArray.forEach((row) =>
             {
-                let newRow = String(row).replace(/,(?!\s)/g, ', ').replace(/"""/,'"');
+                let newRow = String(row + ',').replace(/,(?!\s)/g, ', ').replace(/"""/,'"');
                 let groups = [...newRow.matchAll(regex)];
 
                 groups.forEach((group) =>
                 {
-                    let newGroup = rsc.recursiveReplace(String(group).replace(/^\s*?"/,'').replace(/"\s*?,$/,''), RegExp(/"|,/g), symbols) + ',';
+                    let newGroup = rsc.recursiveReplace(String(group).replace(/^\s*?"/,'').replace(/"\s*?,$/,''), RegExp(/"|,/g), symbols);
                     newRow = String(newRow).replace(group, newGroup);
                 });
 
