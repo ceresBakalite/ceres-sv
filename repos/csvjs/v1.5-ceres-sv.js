8908220b3b5974c4f95d2e10d45c866fbf5a4e77
ceresbakalite <ceres@users.noreply.github.com>
2020-12-06 18:58:27 +1100

    Update v1.5-ceres-sv.js

--- repos/csvjs/v1.5-ceres-sv.js
+++ repos/csvjs/v1.5-ceres-sv.js
@@ -109,14 +109,11 @@ window.ceres = {};
 
         this.parseCSV = function(text, delimeter)
         {
-            //if (!char.quote) char.quote = '&quot;';
-            //if (!char.comma) char.comma = '&comma;';
-            if (!delimeter.quote) delimeter.quote = '_csvq;';
-            if (!delimeter.comma) delimeter.comma = '_csvc;';
+            if (!char.quote) char.quote = '\\x22';
+            if (!char.comma) char.comma = '\\x2c';
 
             const textArray = text.split('\n');
             const regex = /"[^&]*?"[\s]*?,/g;
-            //const symbols = { '"': '_csvq_' , ',': '_csvc_' };
             const symbols = { '"': delimeter.quote, ',': delimeter.comma };
             const newArray = new Array(textArray.length);
 
