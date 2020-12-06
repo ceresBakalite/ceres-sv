fa06566c63be6f116dfe101355b4c956eb1ce855
ceresbakalite <ceres@users.noreply.github.com>
2020-12-07 01:46:10 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -107,28 +107,43 @@ window.ceres = {};
             return doc.body.textContent || doc.body.innerText;
         }
 
+        // Trevor Dixon - Stackoverflow
         this.parseCSV = function(text)
         {
-            let arr = [];
-            let quote = false;
+            var arr = [];
+            var quote = false;  // 'true' means we're inside a quoted field
 
-            for (var row = col = c = 0; c < text.length; c++)
-            {
-                let cc = str[c], nc = str[c+1];
+            // Iterate over each character, keep track of current row and column (of the returned array)
+            for (var row = 0, col = 0, c = 0; c < str.length; c++) {
+                var cc = str[c], nc = str[c+1];        // Current character, next character
+                arr[row] = arr[row] || [];             // Create a new row if necessary
+                arr[row][col] = arr[row][col] || '';   // Create a new column (start with empty string) if necessary
 
-                arr[row] = arr[row] || [];
-                arr[row][col] = arr[row][col] || '';
-
+                // If the current character is a quotation mark, and we're inside a
+                // quoted field, and the next character is also a quotation mark,
+                // add a quotation mark to the current column and skip the next character
                 if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
-                if (cc == '"') { quote = !quote; continue; }
-                if (cc == ',' && !quote) { ++col; continue; }
-                if (cc == '\n' && !quote) { ++row; col = 0; continue; }
 
+                // If it's just one quotation mark, begin/end quoted field
+                if (cc == '"') { quote = !quote; continue; }
+
+                // If it's a comma and we're not in a quoted field, move on to the next column
+                if (cc == ',' && !quote) { ++col; continue; }
+
+                // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+                // and move on to the next row and move to column 0 of that new row
+                if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+                // If it's a newline (LF or CR) and we're not in a quoted field,
+                // move on to the next row and move to column 0 of that new row
+                if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+                if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+                // Otherwise, append the current character to the current column
                 arr[row][col] += cc;
             }
 
             console.log('arr: ' + arr.join('\n'));
-
             return arr;
         }
 
