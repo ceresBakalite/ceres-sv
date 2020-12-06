c4394a2d934655c3eb5d552f86321b803b725b40
ceresbakalite <ceres@users.noreply.github.com>
2020-12-07 02:03:54 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -107,8 +107,78 @@ window.ceres = {};
             return doc.body.textContent || doc.body.innerText;
         }
 
+        var parseCSV = {
+
+            parse: function(csv, reviver) {
+                reviver = reviver || function(r, c, v) { return v; };
+                var chars = csv.split(''), c = 0, cc = chars.length, start, end, table = [], row;
+                while (c < cc) {
+                    table.push(row = []);
+                    while (c < cc && '\r' !== chars[c] && '\n' !== chars[c]) {
+                        start = end = c;
+                        if ('"' === chars[c]){
+                            start = end = ++c;
+                            while (c < cc) {
+                                if ('"' === chars[c]) {
+                                    if ('"' !== chars[c+1]) {
+                                        break;
+                                    }
+                                    else {
+                                        chars[++c] = ''; // unescape ""
+                                    }
+                                }
+                                end = ++c;
+                            }
+                            if ('"' === chars[c]) {
+                                ++c;
+                            }
+                            while (c < cc && '\r' !== chars[c] && '\n' !== chars[c] && ',' !== chars[c]) {
+                                ++c;
+                            }
+                        } else {
+                            while (c < cc && '\r' !== chars[c] && '\n' !== chars[c] && ',' !== chars[c]) {
+                                end = ++c;
+                            }
+                        }
+                        row.push(reviver(table.length-1, row.length, chars.slice(start, end).join('')));
+                        if (',' === chars[c]) {
+                            ++c;
+                        }
+                    }
+                    if ('\r' === chars[c]) {
+                        ++c;
+                    }
+                    if ('\n' === chars[c]) {
+                        ++c;
+                    }
+                }
+                return table;
+            },
+
+            stringify: function(table, replacer) {
+                replacer = replacer || function(r, c, v) { return v; };
+                var csv = '', c, cc, r, rr = table.length, cell;
+                for (r = 0; r < rr; ++r) {
+                    if (r) {
+                        csv += '\r\n';
+                    }
+                    for (c = 0, cc = table[r].length; c < cc; ++c) {
+                        if (c) {
+                            csv += ',';
+                        }
+                        cell = replacer(r, c, table[r][c]);
+                        if (/[,\r\n"]/.test(cell)) {
+                            cell = '"' + cell.replace(/"/g, '""') + '"';
+                        }
+                        csv += (cell || 0 === cell) ? cell : '';
+                    }
+                }
+                return csv;
+            }
+        };
+
         // Trevor Dixon - Stackoverflow
-        this.parseCSV = function(text)
+        this.xxxparseCSV = function(text)
         {
             var arr = [];
             var quote = false;  // 'true' means we're inside a quoted field
@@ -723,8 +793,9 @@ window.ceres = {};
                     this.getFileType = function(textList)
                     {
                         if (rsc.fileType(cfg.src, '.json')) return atr.parseJSON(textList);
-                        if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV(textList, { quote: '_csvq_', comma: '_csvc_'});
-
+                        //if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV(textList, { quote: '_csvq_', comma: '_csvc_'});
+                        if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV.parse(textList);
+parseCSV
                         return textList;
                     }
 
