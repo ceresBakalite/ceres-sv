08f6564454c616c4e5ad13da4be681aea89c6e69
ceresbakalite <ceres@users.noreply.github.com>
2020-12-08 15:44:55 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -744,7 +744,8 @@ window.ceres = {};
                     this.getFileType = function(textList)
                     {
                         if (rsc.fileType(cfg.src, '.json')) return atr.parseJSON(textList);
-                        if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV(textList, { quote: rsc.attrib.quoteSymbol, comma: rsc.attrib.commaSymbol});
+                        //if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV(textList, { quote: rsc.attrib.quoteSymbol, comma: rsc.attrib.commaSymbol});
+                        if (rsc.fileType(cfg.src, '.csv')) return rsc.parseCSV(textList, { quote: '&quot;', comma: '&comma;'});
 
                         return textList;
                     }
