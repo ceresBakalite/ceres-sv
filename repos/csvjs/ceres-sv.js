8e038a66e9498eaed62b40a8aaf517b17e7addb1
ceresbakalite <ceres@users.noreply.github.com>
2020-12-11 02:34:35 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -186,7 +186,7 @@ window.ceres = {};
 
                 });
 
-                jsonString = '[ ' + jsonString.replace(/,\s*?$/, '').replaceAll(symbol.separator, '&comma') + ' ]';
+                jsonString = '[' + jsonString.replace(/,\s*?$/, '').replaceAll(symbol.separator, '&comma') + ']';
 
                 console.log('json: ' + jsonString);
 
