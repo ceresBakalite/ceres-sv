4771b2b6ad61fec1ba03acc99ef18a6cc0a46d71
ceresbakalite <ceres@users.noreply.github.com>
2020-12-12 22:31:05 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -244,7 +244,7 @@ window.ceres = {};
                             {
                                 if (cfg.srcRoot) return 'undefined';
 
-                                let el = cfg.attrib.embed ? document.getElementById(cfg.attrib.embed) : null;
+                                let el = (cfg.attrib.embed) ? document.getElementById(cfg.attrib.embed) : null;
 
                                 if (rsc.ignore(el))
                                 {
@@ -300,11 +300,11 @@ window.ceres = {};
 
                                         const styleAttribute = function(item)
                                         {
-                                            let re = Boolean(item.match(/color:/i)) ? /color[^&]*?;/i
-                                                : Boolean(item.match(/font:/i)) ? /font[^&]*?;/i
-                                                : Boolean(item.match(/padding:/i)) ? /padding[^&]*?;/i
-                                                : Boolean(item.match(/top:/i)) ? /top[^&]*?;/i
-                                                : Boolean(item.match(/bottom:/i)) ? /bottom[^&]*?;/i
+                                            let re = (Boolean(item.match(/color:/i))) ? /color[^&]*?;/i
+                                                : (Boolean(item.match(/font:/i))) ? /font[^&]*?;/i
+                                                : (Boolean(item.match(/padding:/i))) ? /padding[^&]*?;/i
+                                                : (Boolean(item.match(/top:/i))) ? /top[^&]*?;/i
+                                                : (Boolean(item.match(/bottom:/i))) ? /bottom[^&]*?;/i
                                                 : null;
 
                                             if (!rsc.ignore(re))
@@ -373,14 +373,14 @@ window.ceres = {};
                                 let shadowList = function()
                                 {
                                     let text = csvRoot.textContent;
-                                    return !rsc.ignore(text) ? text : null;
+                                    return (!rsc.ignore(text)) ? text : null;
                                 }
 
                                 let lightList = function()
                                 {
                                     rsc.inspect({ type: rsc.attrib.notify, notification: remark.srcSearch, logtrace: cfg.attrib.trace });
 
-                                    let text = cfg.template.tagName == 'TEMPLATE' ? cfg.template.content.textContent : cfg.template.textContent;
+                                    let text = (cfg.template.tagName == 'TEMPLATE') ? cfg.template.content.textContent : cfg.template.textContent;
                                     if (rsc.ignore(text)) return rsc.inspect({ type: rsc.attrib.error, notification: remark.template + ' [' + cfg.attrib.embed + ']' });
 
                                     return text;
@@ -396,7 +396,7 @@ window.ceres = {};
                                 if (!rsc.ignore(imageList))
                                 {
                                     rsc.inspect({ type: rsc.attrib.notify, notification: remark.markup + '[' + (cfg.srcRoot ? csvRoot.id + ' - ' + rsc.fileName(cfg.src) : cfg.attrib.embed + ' - template') + ']' + rsc.attrib.newline + imageList.replaceAll(cfg.commaSymbol, '&comma;'), logtrace: cfg.attrib.trace });
-                                    cfg.imageArray = imageList ? imageList.trim().split('\n') : null;
+                                    cfg.imageArray = (imageList) ? imageList.trim().split('\n') : null;
                                 }
 
                                 return !rsc.ignore(cfg.imageArray);
@@ -413,7 +413,7 @@ window.ceres = {};
                         {
                             const getSwipe = function(swipe)
                             {
-                                let offset = swipe.action ? swipe.right : swipe.left;
+                                let offset = (swipe.action) ? swipe.right : swipe.left;
                                 cfg.slide = cfg.slide += offset;
 
                                 atr.get.slide({ shadow: cfg.shadow });
@@ -513,7 +513,7 @@ window.ceres = {};
                             {
                                 if (!('caches' in window)) return;
 
-                                cfg.shadowsrc = cfg.srcRoot ? cfg.src.split() : Array.from('');
+                                cfg.shadowsrc = (cfg.srcRoot) ? cfg.src.split() : Array.from('');
 
                                 let cacheName = csv + '-cache';
                                 let urlArray = rsc.removeDuplcates(cfg.shadowsrc.concat(cfg.cssRoot.concat([ rsc.attrib.metaUrl ])));
@@ -589,7 +589,8 @@ window.ceres = {};
 
                             cfg.bodyNode.appendChild(imgNode);
 
-                            cfg.imageArray.forEach((item) =>
+                            //for (let item = 0; item < cfg.imageArray.length; item++)
+                            for (let item of cfg.imageArray)
                             {
                                 var arrayItem = cfg.imageArray[item].split(',');
 
@@ -602,7 +603,7 @@ window.ceres = {};
                                 if (cfg.attrib.sur) rsc.composeElement({ type: 'div', parent: slideNode, markup: getSurtitle() }, { class: 'surtitle fade' });
                                 rsc.composeElement({ type: 'img', parent: slideNode }, { class: 'slide', onclick: zoomEvent, src: setURL(), alt: setSubtitle(), loading: setLoading() });
                                 if (cfg.attrib.sub) rsc.composeElement({ type: 'div', parent: slideNode, markup: getSubtitle() }, { class: 'subtitle fade' });
-                            });
+                            }
 
                             rsc.composeElement({ type: 'a', parent: imgNode, markup: '&#10094;' }, { class: atr.getClassList('left'), onclick: this.href });
                             rsc.composeElement({ type: 'a', parent: imgNode, markup: '&#10095;' }, { class: atr.getClassList('right'), onclick: this.href });
@@ -675,8 +676,8 @@ window.ceres = {};
                         json.forEach((node) =>
                         {
                             str += node.url
-                                + node.sub ? ', ' + node.sub.replace(',', cfg.commaSymbol) : ''
-                                + node.sur ? ', ' + node.sur.replace(',', cfg.commaSymbol) : ''
+                                + ((node.sub) ? ', ' + node.sub.replace(',', cfg.commaSymbol) : '')
+                                + ((node.sur) ? ', ' + node.sur.replace(',', cfg.commaSymbol) : '')
                                 + '\n';
                         });
 
@@ -713,7 +714,7 @@ window.ceres = {};
 
                             const nodeName = function(i)
                             {
-                                return symbol.nodes[i] ? '"' + symbol.nodes[i] + '": ' : '"node' + i+1 + '": ';
+                                return (symbol.nodes[i]) ? '"' + (symbol.nodes[i]) + '": ' : '"node' + i+1 + '": ';
                             }
 
                             newArray.forEach((row) => {
