c7e3280ffb53f65c12e15c08b257e880c29c84b0
ceresbakalite <ceres@users.noreply.github.com>
2020-12-13 02:10:37 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -591,6 +591,24 @@ window.ceres = {};
 
                             let index = 0;
 
+                            cfg.imageArray.forEach((item, i) =>
+                            {
+
+                                var arrayItem = cfg.imageArray(item).split(',');
+
+                                let slideNode = document.createElement('div');
+                                slideNode.id = 'img' + i;
+                                slideNode.className = classlist;
+
+                                imgNode.appendChild(slideNode);
+
+                                if (cfg.attrib.sur) rsc.composeElement({ type: 'div', parent: slideNode, markup: getSurtitle() }, { class: 'surtitle fade' });
+                                rsc.composeElement({ type: 'img', parent: slideNode }, { class: 'slide', onclick: zoomEvent, src: setURL(), alt: setSubtitle(), loading: setLoading() });
+                                if (cfg.attrib.sub) rsc.composeElement({ type: 'div', parent: slideNode, markup: getSubtitle() }, { class: 'subtitle fade' });
+
+                            });
+
+                            /*
                             for (let item in cfg.imageArray)
                             {
                                 var arrayItem = cfg.imageArray[item].split(',');
@@ -605,6 +623,7 @@ window.ceres = {};
                                 rsc.composeElement({ type: 'img', parent: slideNode }, { class: 'slide', onclick: zoomEvent, src: setURL(), alt: setSubtitle(), loading: setLoading() });
                                 if (cfg.attrib.sub) rsc.composeElement({ type: 'div', parent: slideNode, markup: getSubtitle() }, { class: 'subtitle fade' });
                             }
+                            */
 
                             rsc.composeElement({ type: 'a', parent: imgNode, markup: '&#10094;' }, { class: atr.getClassList('left'), onclick: this.href });
                             rsc.composeElement({ type: 'a', parent: imgNode, markup: '&#10095;' }, { class: atr.getClassList('right'), onclick: this.href });
