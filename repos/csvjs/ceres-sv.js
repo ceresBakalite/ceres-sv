c076fefb0376746fd1b56f07695643bd061b2c13
ceresbakalite <ceres@users.noreply.github.com>
2020-12-13 19:31:58 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -382,7 +382,7 @@ window.ceres = {};
 
                                     let text = (cfg.template.tagName == 'TEMPLATE') ? cfg.template.content.textContent : cfg.template.textContent;
 
-console.log('text 2: ' + atr.parseText(String(text)));
+console.log('text 2: ' + atr.parseText(String(cfg.template.content.textContent)));
 if (text.includes('&comma;')) console.log('includes &comma symbols');
 
                                     if (rsc.ignore(text)) return rsc.inspect({ type: rsc.attrib.error, notification: remark.template + ' [' + cfg.attrib.embed + ']' });
