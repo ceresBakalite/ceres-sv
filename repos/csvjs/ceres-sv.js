7b56d349b4dbc901d34dbd24e6ce179b59e45bf1
ceresbakalite <ceres@users.noreply.github.com>
2020-12-14 20:50:07 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -200,8 +200,7 @@ window.ceres = {};
                     const remark = {
                         markup     : 'Image list markup ',
                         element    : 'The element attributes ',
-                        srcSearch  : 'The ' + csv + ' src attribute url is unavailable. Searching for the fallback template element in the document body',
-                        tagSearch  : 'There is no \'embed\' elementId available. Looking for the first occurance of a <template> or <noscript> tagname',
+                        tagSearch  : 'The ' + csv + ' src attribute url is unavailable and there is no \'embed\' elementId. Looking for the first occurance of a <template> or <noscript> tagname',
                         properties : 'Error: Unable to find the ' + csv + ' document element',
                         list       : 'Error: Unable to find either the fetch ' + csv + ' nor the fallback template elements',
                         template   : 'Error: Unable to find the fallback template element when searching the document body',
@@ -384,8 +383,6 @@ window.ceres = {};
 
                                 let lightList = function()
                                 {
-                                    rsc.inspect({ type: rsc.attrib.notify, notification: remark.srcSearch, logtrace: cfg.attrib.trace });
-
                                     let text = (cfg.template.tagName == 'TEMPLATE') ? cfg.template.content.textContent : cfg.template.textContent;
                                     if (rsc.ignore(text)) return rsc.inspect({ type: rsc.attrib.error, notification: remark.template + ' [' + cfg.attrib.embed + ']' });
 
