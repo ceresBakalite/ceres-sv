caadde48c6abc4227d3671fca5bdc1a05b54a4d6
ceresbakalite <ceres@users.noreply.github.com>
2020-12-17 20:51:58 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -538,7 +538,7 @@ window.ceres = {};
                             cfg.shadow.appendChild(styleNode);
                         },
 
-                        body: () => {
+                        body: function() {
 
                             const setURL      = () => !rsc.ignore(ar[0]) ? ar[0].trim() : null;
                             const setLoading  = () => Boolean(cfg.attrib.loading.match(/lazy|eager|auto/i)) ? cfg.attrib.loading : 'auto';
