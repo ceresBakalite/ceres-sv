9e13cd737734af020d9d8c27b4374181875cd7de
ceresbakalite <ceres@users.noreply.github.com>
2020-12-18 03:31:49 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -470,7 +470,7 @@ window.ceres = {};
                                     return iteration === complete || (autoslide++, iteration++, false); // stops when complete
                                 }
 
-                                const auto = setInterval(run => {
+                                const auto = setInterval(() => {
 
                                     if (autoCancel()) clearInterval(auto);
                                     atr.get.slide({ autoslide: autoslide-1 });
