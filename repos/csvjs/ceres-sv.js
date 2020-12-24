4c4cd1955516f851ce74754656e979ca70e6ad64
ceresbakalite <ceres@users.noreply.github.com>
2020-12-24 18:36:49 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -156,9 +156,9 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
 
                                     if (name == 'zoom') {
 
-                                        if (/false/i.test(item)) return false;
 console.log('hello from zoom: ' + item);
 
+                                        if (/false/i.test(item)) return false;
 
                                         cfg.node.clickevent = atrArray.length > 1 ? atrArray[1]
                                             : /true/i.test(item) ? null
