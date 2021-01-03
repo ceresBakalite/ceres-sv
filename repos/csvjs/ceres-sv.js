0f34b9ec482d614a8f67cd45a977f11179328faf
ceresbakalite <ceres@users.noreply.github.com>
2021-01-03 13:29:49 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -15,7 +15,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
     const rsc = {}; // the resource object namespace
     rscMethods(); // resource object methods
 
-    globalThis.customElements.define('ceres-sv', class extends HTMLElement { // ceres-sv global class
+    globalThis.customElements.define('CERES-SV', class extends HTMLElement { // ceres-sv global class
 
         async connectedCallback() {
 
@@ -25,7 +25,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
             const cfg = {}; // configuration object namespace
             const atr = {}; // attribute object namespace
 
-            initialise(this); // a csv root node of the DOM subtree
+            initialise(this); // a slideview root node of the DOM subtree
 
             atr.setDisplay.hide();
 
