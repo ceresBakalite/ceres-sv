5b0490144482728459c7df94178801c0d5baf7a7
ceresbakalite <ceres@users.noreply.github.com>
2021-01-09 08:42:43 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -280,7 +280,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
                                 const parseList = () => {
 
                                     return remark.markup + '[' + (cfg.srcRoot ? csvRoot.id + ' - ' + rsc.fileName(cfg.src)
-                                        : csvRoot.id + ' - local nodename: ' + cfg.node.local) + ']' + rsc.newline + imageList
+                                        : csvRoot.id + ' - node name: ' + cfg.node.local) + ']' + rsc.newline + imageList
                                             .replaceAll(cfg.commaSymbol, '&comma;')
                                             .replace(/&lt;/g, '<')
                                             .replace(/&gt;/g, '>');
