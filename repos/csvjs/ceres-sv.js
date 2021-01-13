c40b2c27a3154593507687f729f4207f697ad08b
ceresbakalite <ceres@users.noreply.github.com>
2021-01-13 20:48:50 +1100

    Update ceres-sv.js

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -601,7 +601,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
 
                         const parseGroup = group => {
 
-                            let newGroup = String(group)
+                            const newGroup = String(group)
                                 .replace(reC, '') // remove trailing quotes & commas & whitespace
                                 .replace(reD, ''); // remove leading quotes & whitespace
 
@@ -612,7 +612,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
 
                         const parseRow = row => {
 
-                            let newRow = row.replace(reA, ''); // remove end symbols at the end of a row
+                            const newRow = row.replace(reA, ''); // remove end symbols at the end of a row
                             newRow = newRow.replaceAll(endSymbol, ', '); // replace any remaining end symbols inside character groups with a comma value separator
 
                             return newRow.replace(reF, ', '); // replace comma & surrounding whitespace
