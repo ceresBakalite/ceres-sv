2c5058dd8fcc5411e403db48c91ed61593826922
ceresbakalite <ceres@users.noreply.github.com>
2021-01-17 19:20:16 +1100

    undefined

--- repos/csvjs/ceres-sv.js
+++ repos/csvjs/ceres-sv.js
@@ -152,7 +152,7 @@ globalThis.ceres = {}; // ceres slideview global (actual or proxy) object namesp
                                         fade    : !rsc.getBoolean(factor),
                                         trace   : rsc.getBoolean(factor),
                                         loading : factor || 'auto',
-                                        name    : factor || false,
+                                        name    : factor || 'undefined',
                                         delay   : Number.isInteger(parseInt(factor, 10)) ? parseInt(factor, 10) : 250
                                     };
 
